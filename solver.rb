2feb5e441f37a6dd64e36e3ccba35aa3f16dd0d8
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-06 15:08:22 +0430

    Create the class for unit test

---
+++ solver.rb
@@ -0,0 +1,18 @@
+# Project requirements
+# Create a class called Solver.
+# Create a method called factorial that takes one argument, an integer N, and returns the factorial for that number.
+# The factorial is the multiplication of all integers from 1 to N and has the special case that the factorial of 0 is 1. 
+# This method only accepts 0 and positive integers, so if a negative integer is given it should raise an exception.
+# Create a method called reverse that takes one argument, a string word, and returns word reversed (e.g. if word is "hello" it returns "olleh").
+# Create a method called fizzbuzz that takes one argument, an integer N, and returns a string.
+# The returned string is constructed following these rules:
+# When N is divisible by 3, return "fizz".
+# When N is divisible by 5, return "buzz".
+# When N is divisible by 3 and 5, return "fizzbuzz".
+# Any other case, return N as a string (e.g. say N is 7 then return "7").
+# Remember to start with tests for all of the methods described above. Your commit history should prove that you have used TDD.
+
+class Solver
+  def factorial(N)
+  end
+end    
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require 'yaml'
+require_relative '../solver'
