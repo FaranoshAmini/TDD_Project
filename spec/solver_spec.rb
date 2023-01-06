69cf753a6b531517d19058c276ddc81a010110d0
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-06 11:10:40 +0000

    Write unit tests for the methods that will be needed in the Solver class.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,9 +1,47 @@
-# Create a method called factorial that takes one argument, an integer N, and returns the factorial for that number.
-# The factorial is the multiplication of all integers from 1 to N and has the special case that the factorial of 0 is 1. 
-# This method only accepts 0 and positive integers, so if a negative integer is given it should raise an exception.
-
 require_relative 'spec_helper'
 
 describe Solver do
+  before :each do 
+    @solver = Solver.new
+  end 
   
+  describe "#factorial" do
+  # The method called factorial takes one argument, an integer N, and returns the factorial for that number.
+    it "should return the factorial of a number" do
+      expect(@solver.factorial(0)).to eql(1)
+      expect(@solver.factorial(2)).to eql(24)
+    end
+
+    it "should raise an exception if the number is negative" do
+      expect(@solver.factorial(-1)).to eql("-1 is not a positive number")
+      expect(@solver.factorial(-4).to eql("-4 is not a positive number"))
+    end
+  end
+
+  describe "#reverse" do
+    it "returns the reverse of the text" do
+      expect(@solver.reverse("hello")).to eql("olleh")
+    end
+  end
+
+  describe "#fizzbuzz" do
+# When N is divisible by 3, return "fizz".
+    it "returns the a string representation of an integer divisible by 3" do
+      expect(@solver.fizzbuzz(9)).to eq("fizz")
+    end
+
+# When N is divisible by 5, return "buzz".
+    it "should return a string representation of an integer divisible by 5" do
+      expect(@solver.fizzbuzz(10)).to eq("buzz")
+    end
+  
+# When N is divisible by 3 and 5, return "fizzbuzz".
+    it "should return a string representation of an integer divisible by 3 and 5" do
+      expect(@solver.fizzbuzz(15)).to eq("fizzbuzz")
+    end
+
+# Any other case, return N as a string (e.g. say N is 7 then return "7").
+    it "should return N as a string" do
+      expect(@solver.fizzbuzz(7)).to eq("7")
+    end
 end
